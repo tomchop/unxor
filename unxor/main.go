4de789ff7907a6f474e6bdf7a719a4db0187343d
Thomas Chopitea <tomchop@gmail.com>
2019-08-10 15:24:44 +0200

    Refactor some functions

--- unxor/main.go
+++ unxor/main.go
@@ -9,35 +9,55 @@ import (
 	unxor "github.com/tomchop/unxor/unxorlib"
 )
 
-func main() {
-	fname := flag.String("f", "", "Filename to decrypt")
-	guess := flag.String("g", "", "Known plaintext (string)")
-	guessHex := flag.String("h", "", "Known plaintext (hex encoded)")
+func parseArgs() (*string, *string, *string) {
+	fn := flag.String("f", "", "Filename to decrypt")
+	g := flag.String("g", "", "Known plaintext (string)")
+	gh := flag.String("gh", "", "Known plaintext (hex encoded)")
 	flag.Parse()
 
-	if *fname == "" {
+	if *fn == "" {
 		fmt.Printf("You must specify a filename with the -f flag.")
 		os.Exit(-1)
 	}
 
-	if *guess != "" && *guessHex != "" {
-		fmt.Printf("-g and -h are mutually exclusive.")
+	if *g != "" && *gh != "" {
+		fmt.Printf("-gh and -h are mutually exclusive.")
 		os.Exit(-1)
 	}
 
-	var knownPlaintext []byte
-	if *guessHex != "" {
-		fmt.Printf("Know plaintext (hex): %s\n", *guessHex)
-		knownPlaintext, _ = hex.DecodeString(*guessHex)
-	}
-	if *guess != "" {
-		fmt.Printf("Know plaintext: %s\n", *guess)
-		knownPlaintext = []byte(*guess)
+	if *g == "" && *gh == "" {
+		fmt.Printf("You must specify either -gh or -g.")
+		os.Exit(-1)
 	}
 
-	fmt.Printf("Reading from: %s\n", *fname)
-	data := unxor.ReadFile(*fname)
-	key := unxor.FindKey(data, knownPlaintext)
+	return fn, g, gh
+}
+
+func main() {
+
+	fn, g, gh := parseArgs()
+
+	fmt.Printf("Reading from: %s\n", *fn)
+	data := unxor.ReadFile(*fn)
+
+	var knownPlaintext []byte
+	if *gh != "" {
+		fmt.Printf("Know plaintext (hex): %s\n", *gh)
+		knownPlaintext, _ = hex.DecodeString(*gh)
+	}
+	if *g != "" {
+		fmt.Printf("Know plaintext: %s\n", *g)
+		knownPlaintext = []byte(*g)
+	}
+
+	key, err := unxor.FindKey(data, knownPlaintext)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 	decrypted := unxor.Xor(data, key)
-	unxor.DumpToFile(decrypted, fmt.Sprintf("%s_decrypted", *fname))
+
+	dump := fmt.Sprintf("%s_decrypted", *fn)
+	n := unxor.DumpToFile(decrypted, dump)
+	fmt.Printf("Dumped %d decrypted bytes in %s\n", n, dump)
 }
